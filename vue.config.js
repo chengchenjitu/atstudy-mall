e1fec5a7724a3806d7162704b33a3e48af5d2cf2
chengchenjitu <1639669423@qq.com>
2023-03-27 18:32:56 +0800

    项目创建成功，缺request

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+	// 开发环境 服务器端配置
+    devServer: {
+		// 反向代理
+        proxy: {
+			// 代理路径  http://192.168.1.102/api
+            '/api': {
+				// 代理目标路径  
+                target: 'http://192.168.1.102:8001',
+                // 允许跨域
+                changeOrigin: true,
+                ws: true,
+                pathRewrite: {
+                    '^/api': ''
+                }
+            }
+        }
+    }
+}
